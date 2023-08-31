ce1b69eead58260eb75f72a6be4f6e16fc9de68a
johny56 <khuen897@gmail.com>
2023-08-31 14:30:07 +0700

    add management team page

--- src/about_us/management/index.jsx
+++ src/about_us/management/index.jsx
@@ -6,15 +6,111 @@ export default function ManagementTeam(){
     return (
 
         <div>
-                {/* <div className="bg-cover h-screen bg-[url(https://drive.google.com/uc?export=view&id=1VbqaCvp6OGGFT11eaatz_AqWai8yKYol)]"> */}
-                    <div className="bg-white h-full w-full text-back text-start">
-                    <p>test</p>
-                    <p>test</p>
-                    <p>test</p>
-                    <p>test</p>
-                    <p>test</p>
+            
+           
+            <div className="bg-cover h-full w-auto bg-[url(https://drive.google.com/uc?export=view&id=1rPrxVtBFJ7S0thIXNkvrZRw23qLo1jLA)] text-back text-end">
+                <div className="w-full font-normal pr-24 mr-12 py-16">
+                        <h1 className="text-7xl font-extrabold mb-4 text-[#827717]">Management </h1>
+                        <h2 className="text-7xl font-extrabold">
+                            <a className="text-border-2 border-indigo-600">Team</a>
+                        </h2>
+                    
+                </div>
+            
+                <div className="grid grud-flow-row-dense grid-cols-1">
+                    <div className="flex justify-center">    
+                            <img className="h-auto w-1/4 rounded-lg" src="https://drive.google.com/uc?export=view&id=1dHjOtrI-a38Ru9S013MMoWGUOJQZSwky" alt=""/>
+            
                     </div>
-                {/* </div> */}
+                </div>
+                <div className="grid grid-flow-row-dense grid-cols-2">
+
+                    <div className="flex justify-center">
+                        <img className="h-auto w-2/3 rounded-lg" src="https://drive.google.com/uc?export=view&id=1pbSXuyZ1J2SV4in9JgSulWyve5zsLEOj" alt=""/>
+                        
+                    </div>
+                    
+                    <div className="flex justify-center">
+                        <img className="h-auto w-2/3 rounded-lg" src="https://drive.google.com/uc?export=view&id=1WWCYYvpoddFCt_xOuff03HMRoVPG_EqR" alt=""/>
+                        
+                    </div>
+
+                </div>
+                <div className="grid grid-flow-row-dense grid-cols-3">
+
+                    <div className="flex justify-center">
+                        
+                        <img className="h-auto w-2/3 rounded-lg" src="https://drive.google.com/uc?export=view&id=1xsPPXIccCZfdjPXi7dLW1VbzZYn5mzvV" alt=""/>
+        
+                    </div>
+                    
+                    <div className="flex justify-center">
+                        
+                        <img className="h-auto w-2/3 rounded-lg" src="https://drive.google.com/uc?export=view&id=1ZfG_96Fhx0j_CUAS2Ce9hOZ3oXxQrjKh" alt=""/>
+        
+                    </div>
+
+                    <div className="flex justify-center">
+                        
+                        <img className="h-auto w-2/3 rounded-lg" src="https://drive.google.com/uc?export=view&id=1H69ug1KKq6-G5Q7uqyofWgE0D3PTEtqd" alt=""/>
+        
+                    </div>
+
+                </div>
+
+                <div className="grid grid-flow-row-dense grid-cols-4">
+
+                    <div className="flex justify-center">
+                        
+                        <img className="h-auto w-2/3 rounded-lg" src="https://drive.google.com/uc?export=view&id=1bF78Y3qjvOk-AHA7t4lIkLbOWRTOGK0s" alt=""/>
+        
+                    </div>
+                    
+                    <div className="flex justify-center">
+                        
+                        <img className="h-auto w-2/3 rounded-lg" src="https://drive.google.com/uc?export=view&id=1950AVJQnSrDHmGMwjNytxnMITmumd1ar" alt=""/>
+        
+                    </div>
+
+                    <div className="flex justify-center">
+                        
+                        <img className="h-auto w-2/3 rounded-lg" src="https://drive.google.com/uc?export=view&id=1M0KQ_Px4SjdvNvDdod38NjHQQ1H3MT2b" alt=""/>
+        
+                    </div>
+                    
+                    <div className="flex justify-center">
+                        
+                        <img className="h-auto w-2/3 rounded-lg" src="https://drive.google.com/uc?export=view&id=1oFzZ3oD0zGhjKLxQ0HwCJx5lv6LqzDsR" alt=""/>
+        
+                    </div>
+                </div>
+                <div className="grid grid-flow-row-dense grid-cols-3">
+
+                    <div className="flex justify-center">
+                        
+                        <img className="h-auto w-2/3 rounded-lg" src="https://drive.google.com/uc?export=view&id=1zx6YSaL15xvaWf5w1nx1MqriuaacaOT1" alt=""/>
+        
+                    </div>
+
+                    <div className="flex justify-center">
+                        
+                        <img className="h-auto w-2/3 rounded-lg" src="https://drive.google.com/uc?export=view&id=1RMFXJapuM7y67pD2E18b1vxiMkQXLDzh" alt=""/>
+        
+                    </div>
+
+                    <div className="flex justify-center">
+                        
+                        <img className="h-auto w-2/3 rounded-lg" src="https://drive.google.com/uc?export=view&id=1pi5W2P76OA9GRkJXxn5vo0_BGzEsPMTz" alt=""/>
+        
+                    </div>
+
+                </div>
+            
+                
+                
+            </div>
+            
+            
 
         </div>
 
