a3f4e4d1298209a2cbdc8dfca930df52acc9e8c7
johny56 <https://github.com/johny56/teakbee.git>
2023-09-14 04:10:14 +0700

    company_structure update

--- src/about_us/company_structure/index.jsx
+++ src/about_us/company_structure/index.jsx
@@ -1,34 +1,78 @@
 import React from "react";
 
-export default function CompanyStructure(){
+export default function CompanyStructure() {
+  return (
+    // <div className="relative">
+      <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed z-20">
+      <div className="pt-32 bg-cover h-full bg-[url(https://drive.google.com/uc?export=view&id=16IPET1uaGQKXb75QfNamUhnwdbNzzWY8)] bg-no-repeat z-10">
 
-    return (
+      <div className="absolute inset-0 flex flex-warp pt-24 pl-16">
+        {/* <h1 className="flex flex-col  text-7xl font-extrabold ml-36 text-[#827717]"> */}
+            <img
+                src="https://drive.google.com/uc?export=view&id=1VKII8kkbWUcMpH9spF_oDHCW0AjAaiTb"
+                className="h-1/4 w-1/4"
+                alt=""
+                />
+            <img
+                src="https://drive.google.com/uc?export=view&id=1pVuxYeJQ5V4xVDhd4jdlLGfmAQAPLQE6"
+                className="h-1/4 w-1/4"
+                alt=""
+                />
+        {/* </h1> */}
+        
+      </div>
+      <div className="pt-32 h-5/6 w-auto flex justify-center item-center ">
+          <img
+            src="https://drive.google.com/uc?export=view&id=1UlDI4TNigmnC72dseYHSIg4q73reQyi3"
+            className=""
+            alt=""
+            />
+            </div>
+        </div>
+                  
+        </div>
+    //   </div>
+      
+  );
+}
+
+              
+            
+
+                  
+            
+
+// import React from "react";
+
+// export default function CompanyStructure(){
+
+//     return (
 
        
-        <div className="relative h-screen">
-        <div
-            className="absolute inset-0 bg-cover bg-center contrast-50 brightness-150"
-            style={{ backgroundImage: "url('https://drive.google.com/uc?export=view&id=1iTcb-ltvgL7HPyM7XGiDIjfingUBI3cP')" }}
-        ></div>
-        <div className="absolute inset-0 flex justify-start pt-6">
-            <h1 className="flex flex-col text-7xl font-extrabold mb-4 ml-36 text-[#827717]">
-            <a className="text-border-2 border-indigo-600">company</a>
-            <a className="ml-20 text-border-2 border-indigo-600">structure</a>
-            </h1>
-            <div className="pt-8 m-4 absolute inset-0 flex items-center justify-center">
-             <img
-             src="https://drive.google.com/uc?export=view&id=1zokBDxnqSDfVdEKNmRBCV5PYI9kBexft"
-             className="max-h-full max-w-full"
-             alt=""
-             />
-             </div>
-         </div>
-        </div>
+//         <div className="relative">
+//         <div
+//             className="absolute h-full w-full inset-0 bg-scroll bg-center"
+//             style={{ backgroundImage: "url('https://drive.google.com/uc?export=view&id=16IPET1uaGQKXb75QfNamUhnwdbNzzWY8')" }}
+//         ></div>
+//         <div className="absolute inset-0 flex justify-start ">
+//             <h1 className="flex flex-col text-7xl font-extrabold mb-4 ml-36 text-[#827717]">
+//             <a className="text-border-2 border-indigo-600">company</a>
+//             <a className="ml-20 text-border-2 border-indigo-600">structure</a>
+//             </h1>
+//             <div className="pt-8 m-4 absolute inset-0 flex items-center justify-center">
+//              <img
+//              src="https://drive.google.com/uc?export=view&id=1UlDI4TNigmnC72dseYHSIg4q73reQyi3"
+//              className="max-h-full max-w-full"
+//              alt=""
+//              />
+//              </div>
+//          </div>
+//         </div>
         
 
             
-    );
+//     );
 
-}
+// }
 
-//https://drive.google.com/file/d/1zokBDxnqSDfVdEKNmRBCV5PYI9kBexft/view?usp=sharing  <--struct
+// https://drive.google.com/file/d/1zokBDxnqSDfVdEKNmRBCV5PYI9kBexft/view?usp=sharing  <--struct
