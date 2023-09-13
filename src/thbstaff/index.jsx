ef3604278da4d46f902bb6743a19176c156c40dc
johny56 <https://github.com/johny56/teakbee.git>
2023-09-14 03:32:03 +0700

    tbh staff update

--- src/thbstaff/index.jsx
+++ src/thbstaff/index.jsx
@@ -6,7 +6,7 @@ export default function TBHstaff(){
     return (
 
         <div>
-            <div className="relative h-full w-full overflow-hidden bg-fixed z-20 bg-white">
+            <div className="relative h-full-screen w-full overflow-hidden bg-fixed z-20 bg-white pt-24">
                 <div className="bg-cover h-screen bg-[url(https://drive.google.com/uc?export=view&id=1MU3qS_GfDb6KGM8mVVomyXZps0R2ljnU)] bg-no-repeat z-10">
 
                 </div>
