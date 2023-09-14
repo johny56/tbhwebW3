9a48e39461bedf21c43a43f12efe42674c590509
johny56 <khuen897@gmail.com>
2023-09-14 08:37:09 +0700

    TBHstaff update

--- src/thbstaff/index.jsx
+++ src/thbstaff/index.jsx
@@ -6,9 +6,9 @@ export default function TBHstaff(){
     return (
 
         <div>
-            <div className="relative h-full w-full overflow-hidden bg-fixed z-20 bg-white">
+            <div className="relative h-screen w-full overflow-hidden bg-cover z-20 bg-white">
                 <div className="bg-cover h-screen bg-[url(https://drive.google.com/uc?export=view&id=1MU3qS_GfDb6KGM8mVVomyXZps0R2ljnU)] bg-no-repeat z-10">
-
+                {/* <img src= */}
                 </div>
             </div>
         </div>
