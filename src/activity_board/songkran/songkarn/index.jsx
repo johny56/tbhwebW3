ae1b65b016ac71efc15e2b45b11b0dc3f86e254a
johny56 <khuen897@gmail.com>
2023-09-14 09:15:24 +0700

    contact us update

---
+++ src/activity_board/songkran/songkarn/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import eventSongkran from './event/songkran';
+
+export default function Songkran(){
+
+    return (
+
+        <div>
+            <div className="relative bottom-0 left-0 right-0 top-0 h-max-screen w-full overflow-hidden">
+                <div className="bg-cover bg-[url(https://drive.google.com/uc?export=view&id=1VbqaCvp6OGGFT11eaatz_AqWai8yKYol)]">
+                    <div className=" bg-cover h-screen bg-white text-back text-start mx-16 px-16 py-32">
+                    <p>hello</p>
+                    </div>
+                
+                </div>
+            </div>
+        </div>
+    );
+}
