e13efe131bf22e8b77f027b03edce03e4777c443
johny56 <khuen897@gmail.com>
2023-09-28 05:36:49 +0700

    fixed scale thbstaff page

--- src/thbstaff/index.jsx
+++ src/thbstaff/index.jsx
@@ -1,18 +1,19 @@
-
 import React from "react";
 
-export default function TBHstaff(){
-
-    return (
-
-        
-        <div className="relative h-full w-full overflow-hidden bg-cover z-20 bg-white">
-            <div className="m-2 h-full w-auto overflow-hidden bg-scroll z-20 bg-white pt-24">
-                <div className="bg-cover h-screen bg-[url(https://drive.google.com/uc?export=view&id=1MU3qS_GfDb6KGM8mVVomyXZps0R2ljnU)] bg-no-repeat z-10">
-                {/* <img src= */}
-                </div>
-            </div>
+export default function TBHstaff() {
+  return (
+    <div className="relative h-screen w-screen">
+      <div className="pt-16">
+        <div
+          className="bg-cover h-screen w-full bg-[url(https://drive.google.com/uc?export=view&id=1MU3qS_GfDb6KGM8mVVomyXZps0R2ljnU)] bg-no-repeat z-10"
+          style={{
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          {/* Content for your fullscreen component */}
         </div>
-
-    );
+      </div>
+    </div>
+  );
 }
