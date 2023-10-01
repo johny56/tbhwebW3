a3ee627faeb83b4a6e49216bd5d86ff069189ab0
johny56 <https://github.com/johny56/teakbee.git>
2023-10-02 04:44:24 +0700

    update managementTeam

--- src/about_us/management/index.jsx
+++ src/about_us/management/index.jsx
@@ -9,108 +9,104 @@ export default function ManagementTeam(){
             
            
             <div className="bg-cover h-full w-auto bg-[url(https://drive.google.com/uc?export=view&id=1NPbCPEdJuJHq1nrKQFVCDmWlFOITrLTN)] text-back text-end">
-                <div className="flex flex-col items-end justify-end pr-8 py-4 pt-28">
-
-                        <h1 className="text-4xl font-extrabold mb-4 pr-8 text-[#273F02]">
-                        <a className="text-border-2 border-indigo-600">Management</a>
-                         </h1>
-                         
-                        {/* <img className="h-fit w-3/4 z-10" src="https://drive.google.com/uc?export=view&id=1EbUAFn7RR1F9wHCIvuxtKpyC1s905Cy2" alt=""/> */}
-                        <h2 className="text-5xl font-extrabold pr-8">
-                            <a className="text-border-2 border-indigo-600">Team</a>
-                            {/* <img className="h-fit w-3/4 z-20" src="https://drive.google.com/uc?export=view&id=1YroMJuy1tPWjf5YYr8iIRqd0dgmZNX_K" alt=""/> */}
-                        </h2>
-                    
-                </div>
-            
-                <div className="grid grud-flow-row-dense grid-cols-3">
                 
-                <div className="flex justify-center pl-80 pt-20">
-                        <img className="h-56 w-56 rounded-lg" src="https://drive.google.com/uc?export=view&id=1pbSXuyZ1J2SV4in9JgSulWyve5zsLEOj" alt=""/>
-                        
-                    </div>
-                    <div className="flex justify-center"> 
-                            <img className="h-56 w-56 rounded-lg " src="https://drive.google.com/uc?export=view&id=1dHjOtrI-a38Ru9S013MMoWGUOJQZSwky" alt=""/>
-            
-                    </div>
-                    
-                    <div className="flex justify-center pr-80 pt-20">
-                        <img className="h-56 w-56 rounded-lg" src="https://drive.google.com/uc?export=view&id=1WWCYYvpoddFCt_xOuff03HMRoVPG_EqR" alt=""/>
-                        
-                    </div>
+                <div className="absolute top-0 right-0">
+                    <div className="flex flex-col items-end justify-end pr-8 py-4 pt-28">
 
-                </div>
-                <div className="grid grid-flow-row-dense grid-cols-3">
-
-                    <div className="flex justify-center pl-60">
+                        <img className="h-56 w-4/6 z-10" src="https://drive.google.com/uc?export=view&id=1vvpZZS31z7uXH12jk24HgVoAo_kdXKZ3" alt=""/>
                         
-                        <img className="h-56 w-56 rounded-lg" src="https://drive.google.com/uc?export=view&id=1xsPPXIccCZfdjPXi7dLW1VbzZYn5mzvV" alt=""/>
-        
                     </div>
-                    
-                    <div className="flex justify-center">
-                        
-                        <img className="h-56 w-56 rounded-lg" src="https://drive.google.com/uc?export=view&id=1ZfG_96Fhx0j_CUAS2Ce9hOZ3oXxQrjKh" alt=""/>
-        
-                    </div>
-
-                    <div className="flex justify-center pr-60">
-                        
-                        <img className="h-56 w-56 rounded-lg" src="https://drive.google.com/uc?export=view&id=1H69ug1KKq6-G5Q7uqyofWgE0D3PTEtqd" alt=""/>
-        
-                    </div>
-
-                </div>
-
-                <div className="grid grid-flow-row-dense grid-cols-4">
-
-                    <div className="flex justify-center">
-                        
-                        <img className="h-56 w-56 rounded-lg" src="https://drive.google.com/uc?export=view&id=1bF78Y3qjvOk-AHA7t4lIkLbOWRTOGK0s" alt=""/>
-        
-                    </div>
-                    
-                    <div className="flex justify-center">
-                        
-                        <img className="h-56 w-56 rounded-lg" src="https://drive.google.com/uc?export=view&id=1950AVJQnSrDHmGMwjNytxnMITmumd1ar" alt=""/>
-        
-                    </div>
-
-                    <div className="flex justify-center">
-                        
-                        <img className="h-56 w-56 rounded-lg" src="https://drive.google.com/uc?export=view&id=1M0KQ_Px4SjdvNvDdod38NjHQQ1H3MT2b" alt=""/>
-        
-                    </div>
-                    
-                    <div className="flex justify-center">
-                        
-                        <img className="h-56 w-56 rounded-lg" src="https://drive.google.com/uc?export=view&id=1oFzZ3oD0zGhjKLxQ0HwCJx5lv6LqzDsR" alt=""/>
-        
-                    </div>
-                </div>
-                <div className="grid grid-flow-row-dense grid-cols-3">
-
-                    <div className="flex justify-center">
-                        
-                        <img className="h-56 w-56 rounded-lg" src="https://drive.google.com/uc?export=view&id=1zx6YSaL15xvaWf5w1nx1MqriuaacaOT1" alt=""/>
-        
-                    </div>
-
-                    <div className="flex justify-center">
-                        
-                        <img className="h-56 w-56 rounded-lg" src="https://drive.google.com/uc?export=view&id=1RMFXJapuM7y67pD2E18b1vxiMkQXLDzh" alt=""/>
-        
-                    </div>
-
-                    <div className="flex justify-center">
-                        
-                        <img className="h-56 w-56 rounded-lg" src="https://drive.google.com/uc?export=view&id=1pi5W2P76OA9GRkJXxn5vo0_BGzEsPMTz" alt=""/>
-        
-                    </div>
-
                 </div>
             
+                <div className="flex flex-row h-full w-auto pt-32 justify-center space-x-32">
+                    <div className="pt-32">
+                        <div className="h-96 w-auto flex justify-center mx-4 px-2">
+                            <img className="h-full w-auto rounded-lg" src="https://drive.google.com/uc?export=view&id=1pbSXuyZ1J2SV4in9JgSulWyve5zsLEOj" alt=""/>
+                        </div>
+                    </div>
+                    <div className="h-96 w-auto flex justify-center mx-4 px-2"> 
+                            <img className="h-full w-auto rounded-lg " src="https://drive.google.com/uc?export=view&id=1dHjOtrI-a38Ru9S013MMoWGUOJQZSwky" alt=""/>
+            
+                    </div>
+                    <div className="pt-32">
+                        <div className="h-96 w-auto flex justify-center mx-4 px-2">
+                            <img className="h-full w-auto rounded-lg" src="https://drive.google.com/uc?export=view&id=1WWCYYvpoddFCt_xOuff03HMRoVPG_EqR" alt=""/>
+                            
+                        </div>
+                    </div>
+                </div>
                 
+
+                <div className="flex flex-row h-full w-auto justify-center space-x-32">
+                    <div className="h-96 w-auto flex justify-center mx-4 px-2">
+                        
+                        <img className="h-full w-auto rounded-lg" src="https://drive.google.com/uc?export=view&id=1xsPPXIccCZfdjPXi7dLW1VbzZYn5mzvV" alt=""/>
+        
+                    </div>
+                    
+                    <div className="h-96 w-auto flex justify-center mx-4 px-2">
+                        
+                        <img className="h-full w-auto rounded-lg" src="https://drive.google.com/uc?export=view&id=1ZfG_96Fhx0j_CUAS2Ce9hOZ3oXxQrjKh" alt=""/>
+        
+                    </div>
+                    
+                    <div className="h-96 w-auto flex justify-center mx-4 px-2">
+                        
+                        <img className="h-full w-auto rounded-lg" src="https://drive.google.com/uc?export=view&id=1H69ug1KKq6-G5Q7uqyofWgE0D3PTEtqd" alt=""/>
+        
+                    </div>
+                
+                </div>
+
+                <div className="flex flex-row h-full w-auto justify-center space-x-32">
+
+                    <div className="h-80 w-auto flex justify-center mx-4 px-2">
+                        
+                         <img className="h-full w-auto rounded-lg" src="https://drive.google.com/uc?export=view&id=1bF78Y3qjvOk-AHA7t4lIkLbOWRTOGK0s" alt=""/>
+        
+                    </div>
+                    
+                    <div className="h-80 w-auto flex justify-center mx-4 px-2">
+                        
+                         <img className="h-full w-auto rounded-lg" src="https://drive.google.com/uc?export=view&id=1950AVJQnSrDHmGMwjNytxnMITmumd1ar" alt=""/>
+        
+                    </div>
+
+                    <div className="h-80 w-auto flex justify-center mx-4 px-2">
+                        
+                         <img className="h-full w-auto rounded-lg" src="https://drive.google.com/uc?export=view&id=1M0KQ_Px4SjdvNvDdod38NjHQQ1H3MT2b" alt=""/>
+        
+                    </div>
+                    
+                    <div className="h-80 w-auto flex justify-center mx-4 px-2">
+                        
+                         <img className="h-full w-auto rounded-lg" src="https://drive.google.com/uc?export=view&id=1oFzZ3oD0zGhjKLxQ0HwCJx5lv6LqzDsR" alt=""/>
+        
+                    </div>
+                </div>
+    
+                <div className="flex flex-row h-full w-auto justify-center space-x-32 pb-44">
+                    <div className="h-80 w-auto flex justify-center">
+                        
+                         <img className="h-full w-auto rounded-lg" src="https://drive.google.com/uc?export=view&id=1zx6YSaL15xvaWf5w1nx1MqriuaacaOT1" alt=""/>
+        
+                    </div>
+
+                    <div className="h-80 w-auto flex justify-center">
+                        
+                         <img className="h-full w-auto rounded-lg" src="https://drive.google.com/uc?export=view&id=1RMFXJapuM7y67pD2E18b1vxiMkQXLDzh" alt=""/>
+        
+                    </div>
+
+                    <div className="h-80 w-auto flex justify-center">
+                        
+                         <img className="h-full w-auto rounded-lg" src="https://drive.google.com/uc?export=view&id=1pi5W2P76OA9GRkJXxn5vo0_BGzEsPMTz" alt=""/>
+        
+                    </div>
+
+                </div>
+            
+            
                 
             </div>
             
