5b466fc3d23456c2a7594e48c6a0ba05ad29c64a
johny56 <https://github.com/johny56/teakbee.git>
2023-11-02 21:13:46 +0700

    activity page add

--- src/about_us/company_activity/index.jsx
+++ src/about_us/company_activity/index.jsx
@@ -8,19 +8,18 @@ export default function Activity(){
 
             <div className="w-full h-screen bg-[#FFFFFF] shadow flex flex-col p-8 pt-32 overflow-hidden">
         
-                <div className="grid grid-rows-1 md:grid-cols-3 gap-2 lg:ml-12 gap-0 grid-rows-1 xl:grid-rows-1 ml-16 gap-0">
+                <div className="grid grid-rows-1 md:ml-2 grid-cols-3 gap-2 lg:ml-16 gap-0 grid-rows-1 xl:grid-rows-1 ml-16 gap-0">
 
                     <div className="grid gap-4 justify-center">
-                    <Link to="/path">
-                        <div class="group relative">
+                    <Link to="/activity_board">
+                        <div className="group relative">
                         <img
                             src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/07/06-1.jpg?fit=1030%2C772&ssl=1"
                             alt="Your Alt Text"
-                            class="max-w-full h-96 rounded transition-all duration-300 group-hover:opacity-75"
+                            className="ml-16 max-w-full h-96 rounded transition-all duration-300 group-hover:opacity-75 "
                         />
-                            <div class="hidden group-hover:flex items-center justify-center absolute top-0 left-0 w-full h-full mr-32">
-                                {/* <span class="text-white text-9xl font-black">+</span> */}
-                                <img className="w-auto h-40 flex justify-center item-center mr-16"
+                            <div className="hidden group-hover:flex items-center justify-center absolute top-0 left-0 w-full h-full mr-32">
+                                <img className="w-auto h-40 flex justify-center item-center ml-12"
                                     src="https://drive.google.com/uc?export=view&id=1vLDPRW1ckbl8LdFqK1oo7t7EBX2ZeGCA"
                                 />
                             </div>
@@ -29,10 +28,10 @@ export default function Activity(){
 
                         <div className="flex flex-cols gap-2 mb-72">
                             <div className="flex justify-center">
-                                <img class="h-72 max-w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/06/skr-01.jpg?fit=960%2C720&ssl=1" alt=""/>
+                                <img className="h-72 w-auto rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/07/04-scaled.jpg?fit=1030%2C687&ssl=1" alt=""/>
                             </div>
                             <div className="flex justify-center">
-                                <img className="h-72 w-auto rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/07/04-scaled.jpg?fit=1030%2C687&ssl=1" alt=""/>
+                                <img class="h-56 max-w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/06/skr-01.jpg?fit=960%2C720&ssl=1" alt=""/>
                             </div>
                         </div>
                         
@@ -41,23 +40,23 @@ export default function Activity(){
                     <div className="grid gap-4 justify-center">
                         <div className="flex flex-cols gap-2 pl-4">
                             <div className="flex justify-start">
+                                <img className="h-56 max-w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/09/02-4.jpg?fit=1030%2C686&ssl=1" alt=""/>
+                            </div>
+                            {/* <div className="flex justify-center">
                                 <img className="h-40 max-w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/03/LINE_ALBUM_Chinese-New-Year-2023_230220_83.jpg?fit=1030%2C686&ssl=1" alt=""/>
-                            </div>
-                            <div className="flex justify-center">
-                                <img className="h-40 max-w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/09/02-4.jpg?fit=1030%2C686&ssl=1" alt=""/>
-                            </div>
+                            </div> */}
                         </div>
                         <div className="flex flex-wrap gap-4 p-4">
-                            <div className="flex justify-center ml-12">
-                                <img className="h-72 max-w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2022/05/yrs-1-e1653465777889.png?fit=1030%2C725&ssl=1" alt=""/>
+                            <div className="flex justify-center ml-20">
+                                <img className="h-56 max-w-full rounded-lg" src="https://drive.google.com/uc?export=view&id=18AJQk3aosy7D62EYxaIvwCi6jlVgxaG2" alt=""/>
                             </div>
                             <div className="flex justify-center mb-72">
-                                <img className="h-48 ml-12 w-auto rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2021/04/IMG20201016133652-1-scaled.jpg?fit=1030%2C474&ssl=1" alt=""/>
+                                <img className="h-56 ml-32 max-w-auto rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2022/05/yrs-1-e1653465777889.png?fit=1030%2C725&ssl=1" alt=""/>
                             </div>
                         </div>
                     </div>
 
-                    <div className="flex flex-col text-clip overflow-hidden m-8">
+                    <div className="flex flex-col text-clip overflow-hidden pl-12">
                         <h1 className=" text-6xl font-sans font-tt-hoves text-green-900 dark:text-neutral-50">
                             ACTIVITIES
                         </h1><br></br>
---
+++ src/activity_board/index.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import Navbar from 'components/navbar';
+
+export default function Activity_board(){
+
+    return (
+
+        <div>
+            <nav className="flex flex-row bg-transition fixed top-0 w-full z-20 ml-16">
+                <Navbar />
+            </nav>
+            
+            <div>
+                <div className="relative bg-cover h-screen bg-blue-200 pt-16">
+                    <div className="flex justify-center bg-white h-full w-full text-back mt-96 pt-16">
+                        <div className="flex flex-col gap-8">
+                        <div className="flex flex-rows-3 gap-16">
+                            <div className="bg-green-300 h-96 w-80 pt-2/3 block max-w-sm bg-white border border-gray-200 rounded-lg">
+                                <div className="flex flex-col h-36 w-80 bg-gray-100 flex items-center justify-start group-hover:bottom-0 opacity-0 hover:opacity-100 transform transition-transform duration-300 ease-in-out hover:translate-y-[-55%] mt-80 pt-2">
+                                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white pr-56"> 2023</h5>
+                                    <p className="font-normal text-gray-700 dark:text-gray-400 pr-40">text...........................<br></br>............................<br></br>............................</p>
+                                </div>
+
+                            </div>
+                            <div className="bg-green-300 h-96 w-80 pt-2/3 block max-w-sm bg-white border border-gray-200 rounded-lg">
+                                <div className="flex flex-col h-36 w-80 bg-gray-100 flex items-center justify-start group-hover:bottom-0 opacity-0 hover:opacity-100 transform transition-transform duration-300 ease-in-out hover:translate-y-[-55%] mt-80 pt-2">
+                                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white pr-56"> 2023</h5>
+                                    <p className="font-normal text-gray-700 dark:text-gray-400 pr-40">text...........................<br></br>............................<br></br>............................</p>
+                                </div>
+
+                            </div>
+                            <div className="bg-green-300 h-96 w-80 pt-2/3 block max-w-sm bg-white border border-gray-200 rounded-lg">
+                                <div className="flex flex-col h-36 w-80 bg-gray-100 flex items-center justify-start group-hover:bottom-0 opacity-0 hover:opacity-100 transform transition-transform duration-300 ease-in-out hover:translate-y-[-55%] mt-80 pt-2">
+                                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white pr-56"> 2023</h5>
+                                    <p className="font-normal text-gray-700 dark:text-gray-400 pr-40">text...........................<br></br>............................<br></br>............................</p>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="flex flex-rows-3 gap-16">
+                        <div className="bg-green-300 h-96 w-80 pt-2/3 block max-w-sm bg-white border border-gray-200 rounded-lg">
+                                <div className="flex flex-col h-36 w-80 bg-gray-100 flex items-center justify-start group-hover:bottom-0 opacity-0 hover:opacity-100 transform transition-transform duration-300 ease-in-out hover:translate-y-[-55%] mt-80 pt-2">
+                                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white pr-56"> 2023</h5>
+                                    <p className="font-normal text-gray-700 dark:text-gray-400 pr-40">text...........................<br></br>............................<br></br>............................</p>
+                                </div>
+
+                            </div>
+                            <div className="bg-green-300 h-96 w-80 pt-2/3 block max-w-sm bg-white border border-gray-200 rounded-lg">
+                                <div className="flex flex-col h-36 w-80 bg-gray-100 flex items-center justify-start group-hover:bottom-0 opacity-0 hover:opacity-100 transform transition-transform duration-300 ease-in-out hover:translate-y-[-55%] mt-80 pt-2">
+                                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white pr-56"> 2023</h5>
+                                    <p className="font-normal text-gray-700 dark:text-gray-400 pr-40">text...........................<br></br>............................<br></br>............................</p>
+                                </div>
+
+                            </div>
+                            <div className="bg-green-300 h-96 w-80 pt-2/3 block max-w-sm bg-white border border-gray-200 rounded-lg">
+                                <div className="flex flex-col h-36 w-80 bg-gray-100 flex items-center justify-start group-hover:bottom-0 opacity-0 hover:opacity-100 transform transition-transform duration-300 ease-in-out hover:translate-y-[-55%] mt-80 pt-2">
+                                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white pr-56"> 2023</h5>
+                                    <p className="font-normal text-gray-700 dark:text-gray-400 pr-40">text...........................<br></br>............................<br></br>............................</p>
+                                </div>
+
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <nav className="absolute end-0 flex justify-center inset-x-8 p-4 pb-12" aria-label="Page navigation">
+                    <ul className="list-style-none flex">
+                        <li>
+                        <a
+                            className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+                            href="#"
+                            aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                        </li>
+                        <li>
+                        <a
+                            className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+                            href="#"
+                            >1</a
+                        >
+                        </li>
+                        <li aria-current="page">
+                        <a
+                            className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+                            href="#"
+                            >2</a
+                        >
+                        </li>
+                        <li>
+                        <a
+                            className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+                            href="#"
+                            >3</a
+                        >
+                        </li>
+                        <li>
+                        <a
+                            className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+                            href="#"
+                            aria-label="Next"
+                            ><span aria-hidden="true">&raquo;</span>
+                        </a>
+                        </li>
+                    </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    );
+}
