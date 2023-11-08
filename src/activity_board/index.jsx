942a02028bda0ec56ce94640e5b8a26d4fa6c42a
johny56 <https://github.com/johny56/teakbee.git>
2023-11-08 22:04:42 +0700

    card update

--- src/activity_board/index.jsx
+++ src/activity_board/index.jsx
@@ -1,21 +1,167 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from 'components/navbar';
-import { useState } from "react";
 
-export default function Activity_board(){
+const pageData = [
+    [
+        {
+        imageUrl: 'https://drive.google.com/uc?export=view&id=1R5UA-WR0il_GxollPD_1iKR7Iyp0fv7Z',
+        date: '6 กันยายน 2556',
+        content: 'เมื่อวันที่ 6 กันยายน 2566 ที่ผ่านมานี้ กรมพัฒนาธุรกิจการค้า . . .',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1uV1p8BvjJ7YCt8aV3LVBoJzkqR7sMrRC',
+            date: '11 สิงหาคม 2566',
+            content: 'ผู้จัดการโรงงานสาขาทุ่งสง คุณสมชาย มโนสถาพร พร้อมทีม . . .',
+        },
+        {
+        imageUrl: 'https://drive.google.com/uc?export=view&id=1mLyMYsN_r8_8DcsQL5pgLwu9LaxZdKe5',
+        date: 'Employees’ Birthday Gift',
+        content: 'ในฐานะองกรณ์ที่ให้ความสำคัญกับความสุขในการทำงานและให้ . . .',
+        },
+    ],
+                            // second line
 
-    const [isCardVisible, setCardVisibility] = useState(false);
 
-    const toggleCard = () => {
-        // Toggle the visibility state
-    console.log('Toggle card function called');
-    console.log('isCardVisible:', isCardVisible);
-    setCardVisibility(!isCardVisible);
-    }
+    [
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1E0QGiOlxbvNez0fuJ2dmwATSWCyB9YgW',
+            date: '28 มิถุนายน 2556',
+            content: 'บริษัทยางไทยปักษ์ใต้ ไม่เพียงแต่ให้ความใส่ใจคุณภาพของ . . .',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1wXiE2q9FDaqVAsb-yUtmiC2irq88TmSY',
+            date: '12 เมษายน 2556',
+            content: 'พนักงานและคณะผู้บริหารทุกคนร่วมสนุกสนานกับกิจกรรมวันขึ้นปีใหม่ . . .',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1hcn58n29VCeGM7IyBMnRAFTL6yoSBBbp',
+            date: '2 กุมภาพันธ์ 2556',
+            content: 'กลุ่มบริษัท ยางไทยปักษ์ใต้ จำกัด ประกอบไปด้วย 5 สาขาคือ . . .',
+        },
+    ],
+    
+                            //    ----------------------page2-------------------
+    [
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1aQGNHf8NgD-DqlpsJiDNm0PQjm6uB2P5',
+            date: '20 มกราคม 2556',
+            content:  'กลุ่มบริษัท ยางไทยปักษ์ใต้ จำกัด สำนักงานใหญ่ ฉลองเทศกาลตรุษจีนในวันที่ . . .',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=15OtZ05_KnSZsnbylHV569areZGWTwjVG',
+            date: '11 พฤศจิกายน 2565',
+            content: 'บริษัทยางไทยปักษ์ใต้มีการส่งเสริมให้นำ 5ส มาใช้ โดยมีการจัดการประกวด . . .',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1kzoMY9lRNNNjVOS_zp8oyzwunL5nrJ-T',
+            date: '2-3 พฤศจิกายน  2022',
+            content: 'บริษัท ยางไทยปักษ์ใต้ จำกัด สาขา ทุ่งสง ได้ผ่านการตรวจจากคณะกรรมการประเมินการเข้าประกวก . . .',
+        },
+    ],
+    
+    [
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=13narKpNafHSYxd3_7JCt94t-Esl0XOT2',
+            date: 'Best Supplier',
+            content: 'Best Supplier for Quality Performance Award',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1oZumGmDWMQ0McrRNzStnkvC1wNdSvglk',
+            date: '28 มกราคม 2565',
+            content: 'บริษัท ยางไทยปักษ์ใต้ จำกัด สำนักงานใหญ่ ได้จัดกิจกรรมไหว้ตรุษจีนต้อนรับปีขาล . . .',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=14Ew_4A29zM0qqLEduDmdGpZWwomfC_rd',
+            date: '18 มีนาคม 2021',
+            content: 'BITECH Bangna, Bangkok — TBH Thung Song led by manager K. Somchai Manosathaporn receives 5S Silver Award . . .',
+        },
+    ],
+    //--------------------page3--------------------------
+    [
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1aIPmOxtH0RLnbPIo0i9YAGpRfVXsEofS',
+            date: '24 กุมภาพันธ์ 2021',
+            content:  'TECHNICAL SKILLS TRAINING . . .',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1mzRux4q3KiyPOeLgFmY6UTCMkuuKXS4T',
+            date: '9 กุมภาพันธ์ 2021',
+            content: 'CHINESE NEW YEAR 2021 . . .',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1XmK_upqXI3wrQgZM9PIFW-xyujYQhY6M',
+            date: '4 กุมภาพันธ์ 2021',
+            content: 'LEAN PRODUCTION TRAINING . . .',
+        },
+    ],
+    
+    [
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=13eJ90d2JmypI1R3KaVzC2-Kri_wWyLT0',
+            date: '23 ธันวาคม 2020',
+            content: 'ISO TRAINING',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1CAX3bp_nHZ_FciqKPA3MMbsZzP8OTPys',
+            date: '21 ธันวาคม 2020',
+            content: 'SOFTSKILL TRAINING',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1I7Eq5UacdelopFZVdvRIsAYCc9gwFMq9',
+            date: '17 ธันวาคม 2020',
+            content: 'NEW YEARS PARTY',
+        },
+    ],
+        //--------------------page 4-----------
+    [
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=17ma_TlaEH8VL91YD6-g-U2NPJgT6zfcT',
+            date: '4 ธันวาคม 2020',
+            content: 'FIRE ESCAPE TRAINING',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1947sdUloZpAEIeOtrAxuxsuW0BuxsElW',
+            date: '9 พฤศจิกายน 2020',
+            content: 'OFFICE MERRIT',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1eftEZ0OiJLxMhdJIsuUpAp8kNX0t-Gp1',
+            date: '16 ตุลาคม 2020',
+            content: 'NR PROPERTY TRAINING',
+        },
+    ],
 
-    return (
-        <div>
-            <nav className="flex flex-row bg-transition fixed top-0 w-full z-20">
+    [
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1nA5u3SUlP7lmVXGIdS-f61z7i3BaOp4h',
+            date: '2 ตุลาคม 2020',
+            content: 'ERGONOMIC TRAINING',
+        },
+        {
+            imageUrl: 'https://drive.google.com/uc?export=view&id=1Y_1cy9LLAZsNqoQwe6o6AdnQv2PrjHRz',
+            date: '5 พฤษภาคม 2020',
+            content: 'HEALTHY BODY',
+        },
+    ],
+    [
+        
+    ],
+    
+    
+];
+
+export default function ActivityBoard() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handlePageChange = (page) => {
+    console.log(currentPage);
+    setCurrentPage(page);
+    if(currentPage > 6)setCurrentPage(1);
+  };
+
+  return (
+    <div>
+      <nav className="flex flex-row bg-transition fixed top-0 w-full z-20">
                 <Navbar />
             </nav>
             <div className="h-screen bg-cover bg-[url(https://drive.google.com/uc?export=view&id=18AJQk3aosy7D62EYxaIvwCi6jlVgxaG2)] pt-28">
@@ -28,112 +174,80 @@ export default function Activity_board(){
                     <div className="flex flex-col gap-8">
                     <div className="flex flex-rows-3 gap-16">
 
-                        <div className="bg-cover bg-[url(https://drive.google.com/uc?export=view&id=1zCXE9iWExk02ivAUBez9rM63pk2gG2ct)] h-96 w-80 block max-w-sm border border-gray-200 rounded-lg shadow-lg">
-                            <div className="flex flex-col h-32 w-76 bg-gray-100 flex items-center justify-start group-hover:bottom-0 opacity-0 hover:opacity-100 transform transition-transform duration-300 ease-in-out hover:translate-y-[-52%] mt-80 pt-2 z-10">
-                                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white pr-56"> year...</h5>
-                                <p className="font-normal text-gray-700 dark:text-gray-400 pr-40">text...........................<br></br>............................<br></br>............................</p>
+                        {pageData[currentPage - 1].map((item, index) => (
+                        <div key={index} className="relative h-full w-80 shadow-lg">
+                            <img className="h-56 w-80" src={item.imageUrl} alt="" />
+                            <div className="flex flex-col h-auto w-auto bg-white flex items-start justify-start transform pt-2 pl-4">
+                            <div className="bg-green-800 h-8 w-36 pt-1 pl-2">
+                                <h5 className="text-base font-bold tracking-tight text-white dark:text-white">{item.date}</h5>
+                            </div>
+                            <div className="whitespace-normal w-3/4 h-auto">
+                                <p className="text-base/6 pt-2 ml-2 pb-2 text-black dark:text-gray-400">{item.content}</p>
+                            </div>
                             </div>
                         </div>
+                        ))}
 
-                        <div className="bg-cover bg-[url(https://drive.google.com/uc?export=view&id=18xoG3N2qGkrU65sJLQkqwudj8wW4uUct)] h-96 w-80 block max-w-sm border border-gray-200 rounded-lg shadow-lg">
-                            <div className="flex flex-col h-32 w-76 bg-white flex items-center justify-start group-hover:bottom-0 opacity-0 hover:opacity-100 transform transition-transform duration-300 ease-in-out hover:translate-y-[-52%] mt-80 pt-2">
-                                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white pr-56"> year...</h5>
-                                <p className="font-normal text-gray-700 dark:text-gray-400 pr-40">text...........................<br></br>............................<br></br>............................</p>
-                            </div>
-                        </div>
+                    </div>
 
-                        <div className="bg-cover bg-[url(https://drive.google.com/uc?export=view&id=1dbRy9xKfBKTsmXpdae-1xbmns6Z_O8mS)] h-96 w-80 block max-w-sm border border-gray-200 rounded-lg shadow-lg">
-                            <div className="flex flex-col h-32 w-76 bg-white flex items-center justify-start group-hover:bottom-0 opacity-0 hover:opacity-100 transform transition-transform duration-300 ease-in-out hover:translate-y-[-52%] mt-80 pt-2">
-                                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white pr-56"> year...</h5>
-                                <p className="font-normal text-gray-700 dark:text-gray-400 pr-40">text...........................<br></br>............................<br></br>............................</p>
+                    <div className="flex flex-cols-3 gap-16">
+
+                            {pageData[currentPage - 0].map((item, index) => (
+                            <div key={index} className="relative h-full w-80 shadow-lg">
+                                <img className="h-56 w-80" src={item.imageUrl} alt="" />
+                                <div className="flex flex-col h-auto w-auto bg-white flex items-start justify-start transform pt-2 pl-4">
+                                <div className="bg-green-800 h-8 w-36 pt-1 pl-2">
+                                    <h5 className="text-base font-bold tracking-tight text-white dark:text-white">{item.date}</h5>
+                                </div>
+                                <div className="whitespace-normal w-3/4 h-auto">
+                                    <p className="text-base/6 pt-2 ml-2 pb-2 text-black dark:text-gray-400">{item.content}</p>
+                                </div>
+                                </div>
                             </div>
+                            ))}
                         </div>
 
                     </div>
-                    <div className="flex flex-rows-3 gap-16">
 
-                        <div className="bg-cover bg-[url(https://drive.google.com/uc?export=view&id=1JAxyiSC4o9RFF7Kzb2gWw3_NwTvjiYaF)] h-96 w-80 block max-w-sm border border-gray-200 rounded-lg shadow-lg">
-                            <div className="flex flex-col h-32 w-76 bg-white flex items-center justify-start group-hover:bottom-0 opacity-0 hover:opacity-100 transform transition-transform duration-300 ease-in-out hover:translate-y-[-52%] mt-80 pt-2">
-                                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white pr-56"> year...</h5>
-                                <p className="font-normal text-gray-700 dark:text-gray-400 pr-40">text...........................<br></br>............................<br></br>............................</p>
-                            </div>
-                        </div>
-
-                        <div className="bg-cover bg-[url(https://drive.google.com/uc?export=view&id=173mNAzWjOqZ3kER6gvSMDaTl0K9hWhJX)] h-96 w-80 block max-w-sm border border-gray-200 rounded-lg shadow-lg">
-                            <div className="flex flex-col h-32 w-76 bg-white flex items-center justify-start group-hover:bottom-0 opacity-0 hover:opacity-100 transform transition-transform duration-300 ease-in-out hover:translate-y-[-52%] mt-80 pt-2">
-                                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white pr-56"> year...</h5>
-                                <p className="font-normal text-gray-700 dark:text-gray-400 pr-40">text...........................<br></br>............................<br></br>............................</p>
-                            </div>
-                        </div>
-
-                        <div className="bg-cover bg-[url(https://drive.google.com/uc?export=view&id=1u0LZdjL5HIgiPjZ2HWJS4zXlAGViZXUf)] h-96 w-80 block max-w-sm border border-gray-200 rounded-lg shadow-lg">
-                            <div className="flex flex-col h-32 w-76 bg-white flex items-center justify-start group-hover:bottom-0 opacity-0 hover:opacity-100 transform transition-transform duration-300 ease-in-out hover:translate-y-[-52%] mt-80 pt-2">
-                                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white pr-56"> year...</h5>
-                                <p className="font-normal text-gray-700 dark:text-gray-400 pr-40">text...........................<br></br>............................<br></br>............................</p>
-                            </div>
-                        </div>
-
-                    </div>
-                    </div>
                 </div>
-
-
-                <nav className="mt-72 absolute end-0 flex justify-center inset-x-8 p-4 pb-12" aria-label="Page navigation">
+                    
+            <div className="absolute h-12 w-screen end-0 flex justify-center p-4 ">
                 <ul className="list-style-none flex">
-                    <li>
-                    <a
-                        className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-                        href="#"
-                        aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
-                    </li>
-                    <li>
-                    <a
-                        className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-                        href="#"
-                        >1</a
-                    >
-                    </li>
-                    <li aria-current="page">
-                    <a
-                        className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-                        href="#"
-                        >2</a
-                    >
-                    </li>
-
-                    <li>
-                    <a
-                        className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-                        href="#"
-                        >3</a
-                    >
-                    </li>
-                    <li>
-                    <a
-                        className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-                        href="#"
-                        aria-label="Next"
-                        ><span aria-hidden="true">&raquo;</span>
-                    </a>
-                    </li>
+                <li>
+                                    <button
+                                    onClick={() => handlePageChange(currentPage - 1)}
+                                    className={`relative block rounded bg-transparent px-3 text-sm text-neutral-600 transition-all duration-300 ${
+                                        currentPage === 1 ? 'pointer-events-none text-neutral-300' : 'hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white'
+                                    }`}
+                                    >
+                                    &laquo;
+                                    </button>
+                                </li>
+                                {[1, 2, 3, '...',].map((page) => (
+                                    <li key={page}>
+                                    <button
+                                        onClick={() => handlePageChange(page)}
+                                        className={`relative block rounded bg-transparent px-3 text-sm text-neutral-600 transition-all duration-300 ${
+                                        currentPage === page ? 'bg-neutral-100 dark:text-white dark:bg-neutral-700' : 'hover:bg-neutral-100 dark:hover:bg-neutral-700 dark:hover:text-white'
+                                        }`}
+                                    >
+                                        {page}
+                                    </button>
+                                    </li>
+                                ))}
+                                <li>
+                                    <button
+                                    onClick={() => handlePageChange(currentPage + 1)}
+                                    className={`relative block rounded bg-transparent px-3 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white`}
+                                    >
+                                    &raquo;
+                                    </button>
+                                </li>
                 </ul>
-                </nav>
-    
             </div>
-        </div>
+
         
-    );
-}         
-
-
-{/* 
-<div className="flex items-center justify-center">
-    <button
-        onClick={toggleCard}
-        className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-    >2
-
-    </button>
-</div> */}
+        </div>                            
+    </div>
+  );
+}
