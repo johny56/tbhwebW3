eb550e3e1c49658071309e87f5c3a7b5d8b35398
johny56 <https://github.com/johny56/teakbee.git>
2023-11-23 05:44:09 +0700

    act...

--- src/activity_board/2023/songkran/index.jsx
+++ src/activity_board/2023/songkran/index.jsx
@@ -14,52 +14,48 @@ export default function Songkran(){
                             HAPPY SONGKRAN FESTIVAL
                             </h1>
                             <div className="flex flex-row">
-                                <div className="flex flex-col">
+                                <div className="flex flex-col h-full w-1/2">
                                     <div className="flex flex-col h-1/2 w-full">
                                         <h2 className="text-xl text-green-800 font-sans font-tt-hoves mx-16">
                                             HAPPY SONGKRAN FESTIVAL<br></br>On 12 April 2023 , TBH Head office
                                         </h2><br></br>
-                                        <p className="mx-16">All employees and the management team enjoyed in-house Thai new year activities by showering and blessing the water for the new year.
+                                        <p className="mx-16 indent-5">All employees and the management team enjoyed in-house Thai new year activities by showering and blessing the water for the new year.
                                             Happy Songkran Festival to all our team!
                                         </p>
                                     </div>
                                     <br></br>
                             
-                                    <div className="flex flex-col h-full w-full">
+                                    <div className="flex flex-col h-full w-full pt-56">
                                         <h2 className="text-xl text-green-800 font-sans font-tt-hoves mx-16">
                                             สงกรานต์มหาสนุก<br></br>12 เมษายน 2556 ณ สำนักงานใหญ่ บริษัทยางไทยปักษ์ใต้ จำกัด
                                         </h2><br></br>
-                                        <p className="mx-16">พนักงานและคณะผู้บริหารทุกคนร่วมสนุกสนานกับกิจกรรมวันขึ้นปีใหม่ไทยด้วยการสรงน้ำพระ
+                                        <p className="mx-16 indent-5">พนักงานและคณะผู้บริหารทุกคนร่วมสนุกสนานกับกิจกรรมวันขึ้นปีใหม่ไทยด้วยการสรงน้ำพระ
                                         และรับพรปีใหม่จากผู้บริหาร <br></br>สุขสันต์วันปีใหม่ไทยทุกคน!
                                         </p>
                                     </div>
                                 </div>
-                                <div className="grid grid-flow-row-dense grid-cols-2 grid-rows-3">
+                                <div className="grid grid-flow-row-dense grid-cols-2 grid-rows-3 w-1/2 h-full">
 
                                 <div className="relative overflow-hidden hover:opacity-75 transition p-4">
                                     
-                                    <img className="h-auto max-w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/06/skr-01.jpg?fit=960%2C720&ssl=1" alt=""/>
+                                    <img className="h-auto w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/06/skr-01.jpg?fit=960%2C720&ssl=1" alt=""/>
                                 
                                 </div>
                                 <div className="relative overflow-hidden hover:opacity-75 transition p-4">
                                     
-                                    <img className="h-auto max-w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/06/skr-02.jpg?fit=960%2C720&ssl=1" alt=""/>
-                                    {/* https://drive.google.com/uc?export=view&id=18AJQk3aosy7D62EYxaIvwCi6jlVgxaG2 */}
+                                    <img className="h-auto w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/06/skr-02.jpg?fit=960%2C720&ssl=1" alt=""/>
                                 </div>
 
                                 <div className="relative w-auto h-auto overflow-hidden hover:opacity-75 transition p-4">
-                                    <img className="h-auto max-w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/06/skr-03.jpg?fit=1030%2C773&ssl=1" alt=""/>
-                                    {/* https://drive.google.com/uc?export=view&id=1SKwh20kJ56vJwm3Bxut1Rdm_9b5Rt9Uv */}
+                                    <img className="h-auto w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/06/skr-03.jpg?fit=1030%2C773&ssl=1" alt=""/>
                                 </div>
 
                                 <div className="relative w-auto h-auto overflow-hidden hover:opacity-75 transition p-4">
-                                    <img className="h-auto max-w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/06/skr-04.jpg?fit=1030%2C773&ssl=1" alt=""/>
-                                    {/* https://drive.google.com/uc?export=view&id=16YDqsO5xkNWOa4NjkIKNDnMFnMfXx8Op */}
+                                    <img className="h-auto w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/06/skr-04.jpg?fit=1030%2C773&ssl=1" alt=""/>
                                 </div>
                                 <div className="relative overflow-hidden hover:opacity-75 transition p-4">
                                     
-                                    <img className="h-auto max-w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/06/skr-05.jpg?fit=1030%2C481&ssl=1" alt=""/>
-                                    {/* https://drive.google.com/uc?export=view&id=18AJQk3aosy7D62EYxaIvwCi6jlVgxaG2 */}
+                                    <img className="h-auto w-full rounded-lg" src="https://i0.wp.com/www.teckbeehang.com/wp-content/uploads/2023/06/skr-05.jpg?fit=1030%2C481&ssl=1" alt=""/>
                                 </div>
                                 
 
