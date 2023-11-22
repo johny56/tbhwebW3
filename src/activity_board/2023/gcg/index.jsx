6a70ed10315fa1a481827f7c8547b7efcd262fc5
johny56 <https://github.com/johny56/teakbee.git>
2023-11-23 06:07:08 +0700

    act..

--- src/activity_board/2023/gcg/index.jsx
+++ src/activity_board/2023/gcg/index.jsx
@@ -15,7 +15,7 @@ export default function Gcg(){
                 <div className="bg-white h-full w-2/3 ml-40 pt-8 pb-24">
                     <h2 className="text-bold text-2xl">GOOD CORPORATE GOVERNANCE STANDARD 2023</h2>
                     <h3 className="text-bold text-xl">On 6 September 2023 , The Department of Business Development, Ministry of Commerce</h3>
-                    <p className="pt-4">The Department of Business Development, Ministry of Commerce on September 6th, 2023, conducted a ceremony to honor the 2023 recipients of the Given Award for Good Corporate Governance Standards.
+                    <p className="indent-8 pt-4">The Department of Business Development, Ministry of Commerce on September 6th, 2023, conducted a ceremony to honor the 2023 recipients of the Given Award for Good Corporate Governance Standards.
 Teck Bee Hang Co., Ltd. has been awarded and certified a renowned “Good Corporate Governance Standards 2023” from the Department of Business Development, Ministry of Commerce. Promoting it as a business
 role model in conducting business with integrity, and fairness, towards customers, community, and social responsibility. The Company complies with 6 principles of “Good Corporate Governance Standards”,
 <br/>
@@ -37,7 +37,7 @@ among stakeholders. The Company is dedicated to upholding Good Corporate Governa
 
                     <h2 className="text-bold text-2xl pt-8">มาตรฐานธรรมาภิบาลธุรกิจ</h2>
                     <h3 className="text-bold text-xl">6 กันยายน 2556 ณ กรมพัฒนาธุรกิจการค้า กระทรวงพาณิชย์</h3>
-                    <p className="pt-4">เมื่อวันที่ 6 กันยายน 2566 ที่ผ่านมานี้ กรมพัฒนาธุรกิจการค้า กระทรวงพาณิชย์ ได้จัดพิธีเชิดชูเกียรติ ให้แก่ธุรกิจที่ผ่านการรับรองตามเกณฑ์มาตรฐานธรรมาภิบาลธุรกิจ ประจำปี 2566
+                    <p className="indent-8 pt-4">เมื่อวันที่ 6 กันยายน 2566 ที่ผ่านมานี้ กรมพัฒนาธุรกิจการค้า กระทรวงพาณิชย์ ได้จัดพิธีเชิดชูเกียรติ ให้แก่ธุรกิจที่ผ่านการรับรองตามเกณฑ์มาตรฐานธรรมาภิบาลธุรกิจ ประจำปี 2566
           บริษัท ยางไทยปักษ์ใต้ จำกัด ได้รับหนังสือรับรองและตราเครื่องหมาย “มาตรฐานธรรมาภิบาลธุรกิจ” จากกรมพัฒนาธุรกิจการค้า กระทรวงพาณิชย์ โดยยกให้เป็นต้นแบบธุรกิจที่จะเป็นแบบอย่างให้ธุรกิจอื่นๆ ในความซื่อสัตย์ ยุติธรรม และเป็นธรรม ต่อผู้บริโภค
 ประชาชนทั่วไป รวมทั้งมีความรับผิดชอบต่อสังคม โดย บริษัท ยางไทยปักษ์ใต้ จำกัด ปฏิบัติตามหลักเกณฑ์มาตรฐานธรรมาภิบาลธุรกิจ ซึ่งมีทั้งหมด 6 หลัก ประกอบด้วย หลักนิติธรรม หลักคุณธรรม หลักความโปร่งใส หลักการมีส่วนร่วม หลักความรับผิดชอบ และหลักความคุ้มค่า
           บริษัทฯได้ดำเนินธุรกิจมายาวนานมากว่า 70 ปี ยึดหลักธรรมาภิบาลในการบริหารงานมาโดยตลอด เพื่อสร้างความเชื่อมั่นให้กับผู้มีส่วนได้ส่วนเสียทุกฝ่าย และพร้อมมุ่งมั่นในดำเนินธุรกิจตามหลักธรรมาภิบาลในการบริหารจัดการต่อไป ทั้งนี้จะช่วยสร้างการเติบโต
