976ccd2b925a37a5a129aaddc3748117d8db4573
johny56 <khuen897@gmail.com>
2023-11-23 15:23:35 +0700

    corevalue add copyright

--- src/about_us/core_value/corevaluepage/index.jsx
+++ src/about_us/core_value/corevaluepage/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function CorevaluePage(){
 
@@ -9,7 +10,7 @@ export default function CorevaluePage(){
             <div className="bg-contain bg-no-repeat h-max-screen w-auto bg-green-200 pt-24">
                 <div className="w-full h-auto bg-white">
                     
-                    <div className="flex flex-row m-2 pt-6">
+                    <div className="flex flex-row m-2 pt-36">
                         
                         <div className="h-full w-1/2 item-end justify-end">
                             <h1 className="text-end text-8xl font-bold text-green-900 pr-20">OUR<br/> VISION</h1>
@@ -64,8 +65,35 @@ export default function CorevaluePage(){
 
                     </div>
 
-                    <div className="h-full w-full flex justify-center item-center px-48 pt-24 pb-16">
+                    <div className="h-full w-full flex justify-center item-center px-48 pt-36 pb-16">
                         <img className="h-full w-full" src="https://drive.google.com/uc?export=view&id=1XDwEOmCirIdsJ-0RIaenfKD6ZtP1oaDH" alt=""/>
+                    </div>
+
+                    <div className="h-64 w-full pt-18 p-4">
+                        <div className="flex flex-col text-center">
+                            <h1 className=" text-xl">Teck Bee Hang Co., Ltd.</h1>
+                            <div className="font-sm text-lg text-gray-700">
+                                <p>Address: 157 Niphat Uthit 2 Road, Hatyai, Songkhla 90110, Thailand.</p>
+                                <p>Telephone: +(66) 7435 5307-9, +(66) 7423 5200-5</p>
+                                <p>Fax: +(66) 7423 4729, +(66) 7423 1308</p>
+                                <p>Office Hours: 8:30 – 17:30 (Mon-Fri)</p>
+                                <p>Sales and Marketing: sales@teckbeehang.com</p>
+                                <p>Human Resources: hrd@teckbeehang.com</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="bg-[#273F02] text-white">
+                        <div className="w-full container mx-auto flex flex-col text-center">
+                            <div className="flex flex-row md:flex-row text-center text-lg md:text-left md:justify-between py-6">
+                                
+                                <Link className="font-sans font-tt-hoves text-white dark:text-white hover:underline" to="/Whistleblowing">WHISTLEBLOWIMG</Link>
+                                <a href="#" className="font-sans font-tt-hoves ">SUGGESTION/CUSTOMER COMPLAINTS</a>
+                                <a href="#" className="font-sans font-tt-hoves ">PRIVACY POLICY | COOKIE POLICY</a>
+                                <Link className="font-sans font-tt-hoves text-white dark:text-white hover:underline" to="/tbhstaff">TBH STAFF</Link>
+                                
+                            </div>
+                        </div>
                     </div>
 
                 </div>
