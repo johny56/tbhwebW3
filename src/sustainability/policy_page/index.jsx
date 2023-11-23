9b1a0e80ae7c7ec5d6adadaaab79f50370b4455d
johny56 <khuen897@gmail.com>
2023-11-23 10:04:39 +0700

    policy page update

--- src/sustainability/policy_page/index.jsx
+++ src/sustainability/policy_page/index.jsx
@@ -4,16 +4,90 @@ const PolicyPage = () =>{
 
     return (
         
-        <div className="h-screen w-full bg-lime-900/50">
-            <div className="bg-cover bg-repeat-x h-full w-full bg-center bg-[url(https://drive.google.com/uc?export=view&id=1KVrNviFEcQ02gLS30r00p6NAHkXxgIEL)]">
+        <div className="h-max-screen w-full bg-lime-900/50">
+            <div className="bg-cover bg-repeat-x h-max-screen w-full bg-center bg-[url(https://drive.google.com/uc?export=view&id=1KVrNviFEcQ02gLS30r00p6NAHkXxgIEL)]">
                 <div className="h-32 w-auto flex flex-row justify-center item-center pt-36">
                     <h1 className="font-bold text-8xl text-white">POLICIES</h1>
                 </div>
-                <div className="m-8 pl-56 pt-32">
-                    <div className="bg-transition flex justify-center bg-lime-100/90 h-56 w-96 rounded-2xl">
-                        <div className="flex justify-center bg-green-900/80 h-12 w-96 rounded-full">
+                <div className="m-8 pl-56 pt-24">
+                    <div className="bg-transition flex flex-col justify-start bg-lime-100/90 h-48 w-96 rounded-2xl">
+                        <div className="flex justify-start bg-green-900/80 h-12 w-full rounded-full">
                             <h1 className="text-white text-xl font-bold m-2">GROUP POLICIES(HAC)</h1>
                         </div>
+
+                        <div className="flex flex-row justify-start h-8 w-full py-2">
+                            <svg className="m-1 pt-1 h-8 w-8 text-green-700"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"/>
+                            </svg>
+                            <a href="https://www.teckbeehang.com/wp-content/uploads/2023/08/Sustainability-Policy-SNRSCP-V6-Approved-and-Adopted-on-22-October-2021.pdf" className="m-1 pt-1 font-thin text-green-900/80">Sustainable Natural Rubber Supply Chain Policy</a>
+                        </div>
+
+                        <div className="flex flex-row justify-start h-8 w-full py-4">
+                            <svg className="m-1 pt-1 h-8 w-8 text-green-700"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"/>
+                            </svg>
+                            <a className="m-1 pt-1 font-thin text-green-900/80">Anti-Corruption & Bribery Policy</a>
+                        </div>
+
+                        <div className="flex flex-row justify-start h-8 w-full pt-6">
+                            <svg className="m-1 pt-1 h-8 w-8 text-green-700"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"/>
+                            </svg>
+                            <a href="" className="m-1 pt-1 font-thin text-green-900/80">Whistleblowing Policy</a>
+                        </div>
+
+                    </div>
+                     
+                </div>
+
+                <div className="m-8 pl-56 pt-24">
+                    <div className="bg-transition flex flex-col justify-start bg-lime-100/90 h-80 w-96 rounded-2xl">
+                        <div className="flex justify-start bg-green-900/80 h-12 w-full rounded-full">
+                            <h1 className="text-white text-xl font-bold m-2">GROUP POLICIES(HAC)</h1>
+                        </div>
+
+                        <div className="flex flex-row justify-start h-8 w-full py-2">
+                            <svg className="m-1 pt-1 h-8 w-8 text-green-700"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"/>
+                            </svg>
+                            <a href="https://www.teckbeehang.com/wp-content/uploads/2023/08/Sustainability-Policy-SNRSCP-V6-Approved-and-Adopted-on-22-October-2021.pdf" className="m-1 pt-1 font-thin text-green-900/80">Sustainable Natural Rubber Supply Chain Policy</a>
+                        </div>
+
+                        <div className="flex flex-row justify-start h-8 w-full py-4">
+                            <svg className="m-1 pt-1 h-8 w-8 text-green-700"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"/>
+                            </svg>
+                            <a className="m-1 pt-1 font-thin text-green-900/80">Anti-Corruption & Bribery Policy</a>
+                        </div>
+
+                        <div className="flex flex-row justify-start h-8 w-full pt-6">
+                            <svg className="m-1 pt-1 h-8 w-8 text-green-700"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"/>
+                            </svg>
+                            <a href="" className="m-1 pt-1 font-thin text-green-900/80">Whistleblowing Policy</a>
+                        </div>
+
+                        <div className="flex flex-row justify-start h-8 w-full py-2 pt-6">
+                            <svg className="m-1 pt-1 h-8 w-8 text-green-700"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"/>
+                            </svg>
+                            <a href="https://www.teckbeehang.com/wp-content/uploads/2023/08/Sustainability-Policy-SNRSCP-V6-Approved-and-Adopted-on-22-October-2021.pdf" className="m-1 pt-1 font-thin text-green-900/80">Sustainable Natural Rubber Supply Chain Policy</a>
+                        </div>
+
+                        <div className="flex flex-row justify-start h-8 w-full py-4">
+                            <svg className="m-1 pt-1 h-8 w-8 text-green-700"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"/>
+                            </svg>
+                            <a className="m-1 pt-1 font-thin text-green-900/80">Anti-Corruption & Bribery Policy</a>
+                        </div>
+
+                        <div className="flex flex-row justify-start h-8 w-full pt-6">
+                            <svg className="m-1 pt-1 h-8 w-8 text-green-700"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"/>
+                            </svg>
+                            <a href="" className="m-1 pt-1 font-thin text-green-900/80">Whistleblowing Policy</a>
+                        </div>
+
                     </div>
                      
                 </div>
