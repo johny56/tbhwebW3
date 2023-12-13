aff344ec4a5516c71694bf6f8f9e854b229f26c7
johny56 <https://github.com/johny56/teakbee.git>
2023-12-13 20:11:59 +0700

    h1 award page update

--- src/about_us/award/index.jsx
+++ src/about_us/award/index.jsx
@@ -28,13 +28,13 @@ export default function Award(){
               
               <div className="bg-transition h-2/3 w-full flex flex-col justify-center item-center pt-96 text-end">
 
-                <div className="flex flex-row justify-center item-center h-full w-auto pt-56">
-                    <div className="h-20 w-20 bg-transition">
-                        <h1 className="text-9xl text-white font-bold">A</h1>
+                <div className="flex flex-row justify-center item-center h-full w-auto pt-48">
+                    <div className="absolute left-96 h-80 w-56 bg-transition pb-24 pr-8 m-4">
+                        <img className="h-full w-full " src="https://drive.google.com/uc?export=view&id=1XVWCJEmmmrRgxoKR4344UXuWWZyxlfXY" alt=""/>
                     </div>
-                    <div className="flex flex-col items-center justify-center text-white pt-16">
-                        <h2 className="text-5xl font-bold text-back pr-40">WARDS &</h2>
-                        <h2 className="text-5xl font-bold text-back pt-2 pr-4">CHIEVEMENTS</h2>
+                    <div className="flex flex-col items-center justify-center text-white pt-32">
+                        <h2 className="text-9xl font-bold text-back pr-56">WARDS &</h2>
+                        <h2 className="text-9xl font-bold text-back pt-2 pl-36">CHIEVEMENTS</h2>
                     </div>
 
                 </div>
