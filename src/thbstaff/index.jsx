0bc27b35307d661042f0ed63371872c5fa1573a3
johny56 <https://github.com/johny56/teakbee.git>
2023-12-14 06:22:27 +0700

    tbhstaff update

--- src/thbstaff/index.jsx
+++ src/thbstaff/index.jsx
@@ -2,18 +2,19 @@ import React from "react";
 
 export default function TBHstaff() {
   return (
-    <div className="relative h-screen w-screen">
-      <div className="pt-16">
-        <div
-          className="bg-cover h-screen w-full bg-[url(https://drive.google.com/uc?export=view&id=1MU3qS_GfDb6KGM8mVVomyXZps0R2ljnU)] bg-no-repeat z-10"
-          style={{
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
+        <div className="bg-cover h-screen w-full bg-white bg-no-repeat">
+
+          <div className="relative bg-cover h-screen w-full bg-[url(https://drive.google.com/uc?export=view&id=1MU3qS_GfDb6KGM8mVVomyXZps0R2ljnU)] bg-no-repeat">
+              <div className="absolute top-96 left-80 bg-green-400 h-80 w-64 hover:bg-green-200">
+              </div>
+              <div className="absolute top-96 right-80 bg-green-400 h-80 w-72 hover:bg-green-100">
+              </div>
+              <div className="absolute top-96 inset-x-24 bg-green-400 h-80 w-64 hover:bg-green-300">
+              </div>
+            
+
+          </div>
           {/* Content for your fullscreen component */}
         </div>
-      </div>
-    </div>
   );
 }
