516e5efd68b3b304dac3ec57e1763bb5545ff0bf
johny56 <khuen897@gmail.com>
2023-12-14 10:49:04 +0700

    text title update

--- src/about_us/award/index.jsx
+++ src/about_us/award/index.jsx
@@ -24,17 +24,17 @@ export default function Award(){
 
         {isCardVisible &&(
             
-            <div className="relative bg-cover h-screen w-full bg-[url(https://drive.google.com/uc?export=view&id=17OcFrqE-5HEkzmHXLn5wP6KmcUKK8f05)]">
+            <div className="relative bg-cover h-screen w-full bg-[url(https://drive.google.com/uc?export=view&id=17OcFrqE-5HEkzmHXLn5wP6KmcUKK8f05)] py-1">
               
-              <div className="h-2/3 w-full flex flex-col justify-center item-center pt-96 text-end md:pt-72">
+              <div className="h-2/3 w-full flex flex-col justify-center item-center pt-96 text-end md:pt-64">
 
                 <div className="flex flex-row justify-center item-center h-full w-auto pt-48">
-                    <div className="absolute left-96 h-80 w-56 bg-transition pb-24 pr-8 m-4 md:pl-20 md:pt-12">
-                        <img className="h-full w-full md:h-3/4 md:w-full" src="https://drive.google.com/uc?export=view&id=1XVWCJEmmmrRgxoKR4344UXuWWZyxlfXY" alt=""/>
+                    <div className="absolute top-3/5 left-72 m-4 pr-12 h-80 w-72 bg-transition pb-24">
+                        <img className="h-full w-full" src="https://drive.google.com/uc?export=view&id=1XVWCJEmmmrRgxoKR4344UXuWWZyxlfXY" alt=""/>
                     </div>
                     <div className="flex flex-col items-center justify-center text-white pt-32">
-                        <h2 className="text-9xl font-bold text-back pr-56 md:text-7xl">WARDS &</h2>
-                        <h2 className="text-9xl font-bold text-back pt-2 pl-36 md:text-7xl md:pl-4">CHIEVEMENTS</h2>
+                        <h2 className="text-9xl font-bold text-back pr-48 md:pl-24">WARDS &</h2>
+                        <h2 className="text-9xl font-bold text-back pt-2 pl-32 md:pl-72">CHIEVEMENTS</h2>
                     </div>
 
                 </div>
@@ -43,7 +43,7 @@ export default function Award(){
                  {/* className="absolute inset-x-0 bottom-0"> */}
                     <button
                     // onClick={toggleCard}
-                    className={`absolute inset-x-0 bottom-8 focus:outline-none transition${
+                    className={`absolute inset-x-0 bottom-4 focus:outline-none transition${
                         isCardVisible ? 'top-[8rem] z-20' : ''
                     }`}
                     >
