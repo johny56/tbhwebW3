101424faac8dd7c35ff45165094b5dab7200196c
johny56 <khuen897@gmail.com>
2024-01-05 04:36:37 +0700

    homepage product fix

--- src/our_product/product/index.jsx
+++ src/our_product/product/index.jsx
@@ -25,23 +25,27 @@ export default function Product(){
                 </div>
                 
             
-                <div className="flex flex-row bg-tranparent bg-clip-content p-1 gap-x-12 font-sans font-tt-hoves pt-8 mt-4 m-2">
+                <div className="flex flex-row bg-tranparent bg-clip-content justify-center ml-24 gap-x-4 font-sans font-tt-hoves pt-8 mt-4">
 
-                    <div className="relative bg-gray-200/90 rounded-full h-full w-1/4 flex flex-col text-start invisible md:visible">
-                        <img className="h-full w-full " src={"https://drive.google.com/uc?export=view&id=1IoSB-WGi7DrqQp4VUWjYp8me0uhXdCSu"}/>
-                        <p className="text-center">STR10</p>
+                    <div className="relative h-full w-1/4 flex flex-col invisible md:visible">
+                        <div className="bg-gray-200/90 rounded-full h-72 w-72"></div>
+                        <img className="absolute top-20 left-11 h-auto w-2/4 hover:scale-125" src={"https://drive.google.com/uc?export=view&id=1IoSB-WGi7DrqQp4VUWjYp8me0uhXdCSu"}/>
+                        <p className="text-xl font-sans font-tt-hoves text-green-900 pl-28">STR10</p>
                     </div>
-                    <div className="relative bg-gray-200/90 rounded-full h-full w-1/4 flex flex-col text-start invisible md:visible">
-                        <img className="h-full w-full " src={"https://drive.google.com/uc?export=view&id=1PfKAz9x_8GKbY0VyLYlXj8SCW-mmUrs-"}/>
-                        <p className="text-center">STR20</p>
+                    <div className="relative h-full w-1/4 flex flex-col invisible md:visible">
+                        <div className="bg-gray-200/90 rounded-full h-72 w-72"></div>
+                        <img className="absolute top-20 left-11 h-auto w-2/4 hover:scale-125" src={"https://drive.google.com/uc?export=view&id=1PfKAz9x_8GKbY0VyLYlXj8SCW-mmUrs-"}/>
+                        <p className="text-xl font-sans font-tt-hoves text-green-900 pl-28">STR20</p>
                     </div>
-                    <div className="relative bg-gray-200/90 rounded-full h-full w-1/4 flex flex-col text-start invisible md:visible">
-                        <img className="h-full w-full " src={"https://drive.google.com/uc?export=view&id=1-GU2ZF2o5QznCUiB6TFncXS6ccz4SLjO"}/>
-                        <p className="text-center">STR20CV</p>
+                    <div className="relative h-full w-1/4 flex flex-col invisible md:visible">
+                        <div className="bg-gray-200/90 rounded-full h-72 w-72"></div>
+                        <img className="absolute top-20 left-11 h-auto w-2/4 hover:scale-125" src={"https://drive.google.com/uc?export=view&id=1-GU2ZF2o5QznCUiB6TFncXS6ccz4SLjO"}/>
+                        <p className="text-xl font-sans font-tt-hoves text-green-900 pl-28">STR20CV</p>
                     </div>
-                    <div className="relative bg-gray-200/90 rounded-full h-full w-1/4 flex flex-col invisible md:visible">
-                        <img className="h-full w-full" src={"https://drive.google.com/uc?export=view&id=1y_Mv63WCCTfhymINmR8uvQ0ptdS_Kjc4"}/>
-                        <p className="text-center">Mixture</p>
+                    <div className="relative h-full w-1/4 flex flex-col invisible md:visible">
+                        <div className="bg-gray-200/90 rounded-full h-72 w-72"></div>
+                        <img className="absolute top-20 left-11 h-auto w-2/4 hover:scale-125" src={"https://drive.google.com/uc?export=view&id=1y_Mv63WCCTfhymINmR8uvQ0ptdS_Kjc4"}/>
+                        <p className="text-xl font-sans font-tt-hoves text-green-900 pl-28">Mixture</p>
                     </div>
 
                 </div>
