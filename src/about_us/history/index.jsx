5370b3efdc4078dd9e31aeda01db40bef3358cd4
johny56 <khuen897@gmail.com>
2024-01-05 14:14:08 +0700

    history update

--- src/about_us/history/index.jsx
+++ src/about_us/history/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function History(){
 
@@ -7,9 +8,10 @@ export default function History(){
 
             <div className="relative h-screen w-full bg-white pt-32">
                 
-                <div className="h-12 w-full flex flex-col justify-center text-center my-8">
-                    <h1 className="h-full w-full font-bold text-black text-6xl">LEE INN TONG</h1>
-                    <p className="h-full w-full font-sans font-tt-hoves text-black text-lg pt-2">- FOUNDER OF TECK BEE HANG -</p>
+                <div className="h-28 w-3/5 flex flex-col justify-center text-center my-4 ml-56 pl-56">
+                    <img className="h-full w-auto rounded-lg m-16" src="https://drive.google.com/uc?export=view&id=1QS_Mf-VzBKZn7-iGL1VJWVlHEFUNGYyd" alt=""/>
+                    {/* <h1 className="h-full w-full font-bold text-black text-6xl">LEE INN TONG</h1>
+                    <p className="h-full w-full font-sans font-tt-hoves text-black text-lg pt-2">- FOUNDER OF TECK BEE HANG -</p> */}
                 </div>
 
                 <div className="flex flex-rows-3 h-2/3 w-full gap-x-4 p-1">
@@ -61,10 +63,252 @@ export default function History(){
                 </div>
 
                 
-                <div className="h-full w-full pt-8 bg-green-200/70">
+                <div className="bg-contain h-5/6 w-full mt-16 bg-[url('https://drive.google.com/uc?export=view&id=11lbVo54ir_Qg6dTM_DH-NBqto86Cc8nK')]">
+
+                    <div className="flex flex-row h-full w-full pl-16">
+                        <div className="h-full w-2/3 m-4">
+                            <div className="h-96 w-auto bg-gray-200/20 border-4 mt-16 ml-12"></div>
+                        </div>
+                        <div className="h-full w-2/5 mt-12 pl-36">
+                            <img className="h-5/6 w-auto" src="https://drive.google.com/uc?export=view&id=1NwoIfPdj-bv6b3j3kQ5_HUPGfMgel6hS" alt=""/>
+                            
+                        </div>
+                    </div>
 
                 </div>
 
+                <div className="bg-contain max-h-screen w-full mt-24 bg-[url('https://drive.google.com/uc?export=view&id=1dU0MhmIxow1DWaytqd7Rv3j4ljqyGlCQ')]">
+                    
+                    <div className="h-36 w-3/6 flex flex-col my-4 ml-96 pl-96 scale-125">
+                        <img className="h-full w-auto rounded-lg m-16" src="https://drive.google.com/uc?export=view&id=1AmvMhJEkGUYAIz3DXaqNh6_eF1wJfe9g" alt=""/>
+                    </div>
+
+                    {/* <!-- component --> */}
+                    <div className="relative container">
+                            <div
+                                className="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50"
+                            >
+                                {/* <!-- right --> */}
+                                <div className="flex md:contents">
+                                <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                                    <div className="h-full w-6 flex items-center justify-center">
+                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                                    </div>
+                                    <div
+                                    className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-800"
+                                    ></div>
+                                </div>
+                                <div
+                                    className="bg-transition col-start-6 col-end-8 p-4 my-4 mr-auto"
+                                >
+                                    <h3 className="font-bold text-3xl mb-1 text-green-900">1954</h3>
+                                    <p className="leading-tight text-base line-clamp-3 text-gray-900">
+                                    Established Company with RSS smoke-houses in Songkhla, Yala & Narathiwas by Mr.Lee In Tong.
+                                    </p>
+                                </div>
+                                </div>
+                                {/* <!-- left --> */}
+                                <div className="flex flex-row-reverse md:contents">
+                                <div
+                                    className="bg-transition col-start-3 col-end-5 p-4 my-4 ml-auto text-end"
+                                >
+                                    <h3 className="font-bold text-3xl mb-1 text-green-900">1966</h3>
+                                    <p className="leading-tight text-base line-clamp-2 text-gray-900">
+                                    Overtook Krung Sang Rubber to become Thailand's leading natural rubber exporter.
+                                    </p>
+                                </div>
+                                <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                                    <div className="h-full w-6 flex items-center justify-center">
+                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                                    </div>
+                                    <div
+                                    className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-800"
+                                    ></div>
+                                </div>
+                                </div>
+                                {/* <!-- right --> */}
+                                <div className="flex md:contents">
+                                <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                                    <div className="h-full w-6 flex items-center justify-center">
+                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                                    </div>
+                                    <div
+                                    className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-800"
+                                    ></div>
+                                </div>
+                                <div
+                                    className="bg-transition col-start-6 col-end-8 p-4 my-4 mr-auto"
+                                >
+                                    <h3 className="font-bold text-3xl mb-1 text-green-900">1973</h3>
+                                    <p className="leading-tight text-base line-clamp-3 text-gray-900">
+                                    Pioneered the production of Thai Tested Rubber and become a well-known world leading exporter of natural rubber.
+                                    </p>
+                                </div>
+                                </div>
+                                {/* <!-- left --> */}
+                                <div className="flex flex-row-reverse md:contents">
+                                <div
+                                    className="bg-transition col-start-3 col-end-5 p-4 my-4 ml-auto text-end"
+                                >
+                                    <h3 className="font-bold text-3xl mb-1 text-green-900">1995</h3>
+                                    <p className="leading-tight text-base line-clamp-3 text-gray-900">
+                                    Honored by Thai Ministry of Agriculture and Cooperatives for its contribution to the Thai Rubber Industry.
+                                    </p>
+                                </div>
+                                <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                                    <div className="h-full w-6 flex items-center justify-center">
+                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                                    </div>
+                                    <div
+                                    className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-800"
+                                    ></div>
+                                </div>
+                                </div>
+                                {/* <!-- right --> */}
+                                <div className="flex md:contents">
+                                <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                                    <div className="h-full w-6 flex items-center justify-center">
+                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                                    </div>
+                                    <div
+                                    className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-800"
+                                    ></div>
+                                </div>
+                                <div
+                                    className="bg-transition col-start-6 col-end-8 p-4 my-4 mr-auto"
+                                >
+                                    <h3 className="font-bold text-3xl mb-1 text-green-900">1999</h3>
+                                    <p className="leading-tight text-gray-900 text-base line-clamp-7">
+                                    Mr.Lee In Tong, chairman of TBH, was honored as an outstanding person in the Thai Rubber Industry,for the
+                                    advancemanets in rubber processing and trading, by the prime minister on the occasion of the 100th Anniversary
+                                    of the Thai Natural Rubber Industry
+                                    </p>
+                                </div>
+                                </div>
+                                {/* <!-- left --> */}
+                                <div className="flex flex-row-reverse md:contents">
+                                <div
+                                    className="bg-transition col-start-3 col-end-5 p-4 my-4 ml-auto text-end"
+                                >
+                                    <h3 className="font-bold text-3xl mb-1 text-green-900">2003</h3>
+                                    <p className="leading-tight text-base line-clamp-2 text-gray-900">
+                                    Production of premium grade STR, meeting consumer specifications.
+                                    </p>
+                                </div>
+                                <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                                    <div className="h-full w-6 flex items-center justify-center">
+                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                                    </div>
+                                    <div
+                                    className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-800"
+                                    ></div>
+                                </div>
+                                </div>
+                                {/* <!-- right --> */}
+                                <div className="flex md:contents">
+                                <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                                    <div className="h-full w-6 flex items-center justify-center">
+                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                                    </div>
+                                    <div
+                                    className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-800"
+                                    ></div>
+                                </div>
+                                <div
+                                    className="relative bg-transition col-start-6 col-end-8 p-4 my-4 mr-auto"
+                                >
+                                    <h3 className="font-bold text-3xl mb-1 text-green-900">2010</h3>
+                                    <p className="leading-tight text-base line-clamp-2 text-gray-900">
+                                    GMG Global Ltd. become majority shareholder of TBH
+                                    </p>
+                                    <div className="absolute h-72 w-auto -top-56 -right-72 bg-transition">
+                                        <img className="h-full w-full" src="https://drive.google.com/uc?export=view&id=12LykCbcYIorsyN3lVejtPiQV82r6Rxnn" alt=""/>
+                                    </div>
+                                </div>
+                                </div>
+                                {/* <!-- left --> */}
+                                <div className="flex flex-row-reverse md:contents">
+                                <div
+                                    className="relative bg-transition col-start-3 col-end-5 p-4 my-4 ml-auto text-end"
+                                >
+                                    <h3 className="font-bold text-3xl mb-1 text-green-900">2016</h3>
+                                    <p className="leading-tight text-base line-clamp-3 text-gray-900">
+                                    Halcyon Agri Corporation Ltd., takeover of GMG Global Ltd., Thus becoming a majority shareholder of TBH.
+                                    </p>
+                                    <div className="absolute h-96 w-auto -top-56 -left-96 bg-transition">
+                                        <img className="h-full w-full" src="https://drive.google.com/uc?export=view&id=1ryU8Q1qqbF-qR3WQPRGVo3CJbcRhIA49" alt=""/>
+                                    </div>
+                                    
+                                </div>
+                                <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                                    <div className="h-full w-6 flex items-center justify-center">
+                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                                    </div>
+                                    <div
+                                    className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-800"
+                                    ></div>
+                                </div>
+                                </div>
+
+                                <div className="absolute h-72 w-auto -top-20 left-48 bg-transition">
+                                    <img className="h-full w-full" src="https://drive.google.com/uc?export=view&id=11ECxqqCM3Dm5O95CflF9eYAJF6b4YOj1" alt=""/>
+                                </div>
+
+                                <div className="absolute h-64 w-56 top-56 -scale-x-100 -right-5 bg-transition">
+                                    <img className="h-full w-full" src="https://drive.google.com/uc?export=view&id=1whVsBu6At7VZnxO_W85bv65p-v6sKeeF" alt=""/>
+                                </div>
+                                <div className="absolute h-96 w-auto top-80 -scale-x-100 -right-5 bg-transition">
+                                    <img className="h-full w-full" src="https://drive.google.com/uc?export=view&id=1nnFzJpdcDNSKhC2vcDb9E4-chLhgTZt4" alt=""/>
+                                </div>
+                                
+                                <div className="absolute h-64 w-auto top-96 mt-64 left-20 bg-transition">
+                                    <img className="h-full w-full" src="https://drive.google.com/uc?export=view&id=1er3QIUSptDjk3WS8YgzkXr2TgyKsVLe-" alt=""/>
+                                </div>
+
+                                
+
+                            </div>
+
+                            
+                            <div className="relative container mt-4 ml-16 h-96 w-full bg-[url('https://drive.google.com/uc?export=view&id=1dU0MhmIxow1DWaytqd7Rv3j4ljqyGlCQ')] z-10 ">
+                                
+                                <div className="h-64 w-full pt-20 p-4">
+                                    <div className="flex flex-col text-center">
+                                        <h1 className=" text-xl">Teck Bee Hang Co., Ltd.</h1>
+                                        <div className="font-sm text-lg text-slate-700">
+                                            <p>Address: 157 Niphat Uthit 2 Road, Hatyai, Songkhla 90110, Thailand.</p>
+                                            <p>Telephone: +(66) 7435 5307-9, +(66) 7423 5200-5</p>
+                                            <p>Fax: +(66) 7423 4729, +(66) 7423 1308</p>
+                                            <p>Office Hours: 8:30 – 17:30 (Mon-Fri)</p>
+                                            <p>Sales and Marketing: sales@teckbeehang.com</p>
+                                            <p>Human Resources: hrd@teckbeehang.com</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="bg-[#273F02] text-white mt-10">
+                                    <div className="w-full container mx-auto flex flex-col text-center">
+                                        <div className="flex flex-row mx-8 md:flex-row text-center text-lg md:text-left md:justify-between py-6">
+                                            
+                                            <Link className="font-sans font-tt-hoves text-white dark:text-white hover:underline" to="/Whistleblowing">WHISTLEBLOWIMG</Link>
+                                            <a href="#" className="font-sans font-tt-hoves ">SUGGESTION/CUSTOMER COMPLAINTS</a>
+                                            <Link to="/policy_page" className="font-sans font-tt-hoves ">PRIVACY POLICY | COOKIE POLICY</Link>
+                                            <Link className="font-sans font-tt-hoves text-white dark:text-white hover:underline" to="/tbhstaff">TBH STAFF</Link>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className="absolute top-72 mt-72 pt-96 -right-20 h-1/2 w-auto z-0">
+                                <img className="h-full w-full" src="https://drive.google.com/uc?export=view&id=1zjGEOdM_2wz8Gig99_WEUmfhSYe9s-V0" alt=""/>
+                            </div>
+
+                           
+
+                </div>
+                </div>
 
             
             </div>
