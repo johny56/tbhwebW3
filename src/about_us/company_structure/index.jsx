a07a1367b246c7fba49869f8ecdfd58695cb5c51
johny56 <khuen897@gmail.com>
2024-02-01 05:37:28 +0700

    companyStructure img update

--- src/about_us/company_structure/index.jsx
+++ src/about_us/company_structure/index.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import Navbar from "components/navbar";
 
+import img1 from "E:/Web/assets/04-Company Structure/BG_Building.jpg";
+import img2 from "E:/Web/assets/04-Company Structure/Company ST..png";
+import img3 from "E:/Web/assets/04-Company Structure/Shareholder structure.png";
+// import img4 from "E:/Web/assets/04-Company Structure/TBH.png";
+
+export const image01 = img1;
+export const image02 = img2;
+export const image03 = img3;
+// export const image04 = img4;
+
 export default function CompanyStructure() {
   return (
     // <div className="relative">
@@ -9,32 +19,36 @@ export default function CompanyStructure() {
         <nav className="flex flex-row bg-transition fixed top-0 w-full z-20">
           <Navbar />
         </nav>
-      <div className="pt-32 bg-cover h-full bg-[url(https://drive.google.com/uc?export=view&id=16IPET1uaGQKXb75QfNamUhnwdbNzzWY8)] bg-no-repeat z-10">
+      <div className="bg-cover h-screen bg-no-repeat z-10">
+      {/* bg-[url(https://drive.google.com/uc?export=view&id=16IPET1uaGQKXb75QfNamUhnwdbNzzWY8)] */}
+        <img src={image01}/>
 
-      <div className="absolute inset-0 flex flex-row pt-28 pl-8">
+      <div className="absolute inset-0 flex flex-col justify-center pl-8">
         {/* <h1 className="flex flex-col  text-7xl font-extrabold ml-36 text-[#827717]"> */}
             <img
-                src="https://drive.google.com/uc?export=view&id=19ePNijrJLKrJ4O8X9FNCEcVyk9XKKYgV"
-                className="h-min w-1/3"
+                // src="https://drive.google.com/uc?export=view&id=1vDVjmb2SGxNkH5vhA7uYTMcgXck_wsPc"
+                src={image03}
+                className="absolute -top-96 right-0 h-min w-auto scale-75"
                 alt=""
                 />
             <img
-                src="https://drive.google.com/uc?export=view&id=1vDVjmb2SGxNkH5vhA7uYTMcgXck_wsPc"
-                className="h-min w-1/3"
+                // src="https://drive.google.com/uc?export=view&id=19ePNijrJLKrJ4O8X9FNCEcVyk9XKKYgV"
+                src={image02}
+                className="absolute top-48 right-72 h-3/4 w-auto p-4"
                 alt=""
                 />
         {/* </h1> */}
         
       </div>
-      <div className="mt-28 h-4/5 w-auto flex justify-center item-center ">
+      {/* <div className="mt-28 h-4/5 w-auto flex justify-center item-center ">
           <img
-            // src="https://drive.google.com/uc?export=view&id=1UlDI4TNigmnC72dseYHSIg4q73reQyi3"
-            src="https://drive.google.com/uc?export=view&id=16zQqLt1ik1E3FHUPY-bPy4TGEeyOVdOe"
+            // src="https://drive.google.com/uc?export=view&id=16zQqLt1ik1E3FHUPY-bPy4TGEeyOVdOe"
+            src={image04}
             className=""
             alt=""
             />
             </div>
-        </div>
+        </div> */}
                   
         </div>
     //   </div>
