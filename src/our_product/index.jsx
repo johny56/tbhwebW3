85f1348d8fe5d8b0de53dd17fa2401258c0441a7
johny56 <khuen897@gmail.com>
2024-02-01 04:38:09 +0700

    specapproved add img

--- src/our_product/index.jsx
+++ src/our_product/index.jsx
@@ -1,16 +1,23 @@
 import React, {useState} from "react";
-// import { Link } from "react-router-dom";
 import Packing from "./packing";
-// import Product_Roll from "./product _roll/carousel";
 import Carousel from "./product _roll/carousel";
+
+import img1 from 'E:/Web/assets/08-Our Product/Products/RRIT-SPEC.webp';
+import img2 from 'E:/Web/assets/08-Our Product/Products/INE-20-SPEC.webp';
+
+// import { Link } from "react-router-dom";
+// import Product_Roll from "./product _roll/carousel";
 // import Product_Rolltest from "./product _roll/test";
 // import Carousel from "./component_p/Carousel";
+export const image01 = img1;
+export const image02 = img2;
 
   const PRITspec = [
     [
       {
           name:'PRIT',
-          imageUrl: 'https://drive.google.com/uc?export=view&id=1YzxZ2MirclJyI98j0oY1JFISAuIrdCxG',
+          // imageUrl: 'https://drive.google.com/uc?export=view&id=1YzxZ2MirclJyI98j0oY1JFISAuIrdCxG',
+          imageUrl: {img1},
           
       },
       
@@ -22,7 +29,8 @@ import Carousel from "./product _roll/carousel";
     [
       {
         name:'INE',
-        imageUrl: 'https://drive.google.com/uc?export=view&id=1vc104kTA6ah_F2FuGEicJCsYNKCJR7dm',
+        // imageUrl: 'https://drive.google.com/uc?export=view&id=1vc104kTA6ah_F2FuGEicJCsYNKCJR7dm',
+        imageUrl: {img2},
         
       },
     ],
@@ -33,19 +41,19 @@ import Carousel from "./product _roll/carousel";
   function Productpage(){
     
     
-        const [currentProduct, setCurrentProduct] = useState(5);
+        // const [currentProduct, setCurrentProduct] = useState(5);
         const [currentSpec, setCurrentSpec] = useState(1);
 
-        const handleCardChange = (product) => {
+        // const handleCardChange = (product) => {
           
-          setCurrentProduct(product);
-          if(currentProduct <= 1){
-            setCurrentProduct(product + 5);
-          }else if(currentProduct > 7){
-            setCurrentProduct(product - 5);
-          }
-          console.log(currentProduct);
-        };
+        //   setCurrentProduct(product);
+        //   if(currentProduct <= 1){
+        //     setCurrentProduct(product + 5);
+        //   }else if(currentProduct > 7){
+        //     setCurrentProduct(product - 5);
+        //   }
+        //   console.log(currentProduct);
+        // };
 
         const handleSpecChange = (spec) => {
           console.log(currentSpec);
@@ -62,7 +70,9 @@ import Carousel from "./product _roll/carousel";
                 <div className="flex flex-col w-full h-full pt-24">
                 
 
-                  <div className="bg-contain bg-no-repeat h-full w-full bg-[url(https://drive.google.com/uc?export=view&id=1YuEwCgokxtAKC82Hxb3H0ATiP3QOS_LJ)]">
+                  {/* <div className="bg-contain bg-no-repeat h-full w-full bg-[url(https://drive.google.com/uc?export=view&id=1YuEwCgokxtAKC82Hxb3H0ATiP3QOS_LJ)]"> */}
+                    
+                  <div className="bg-contain bg-no-repeat h-full w-full bg-[url(E:/Web/assets/08-Our Product/Products/bg.png)]">
 
                       <div className="flex flex-col h-24 mt-36 pt-14">
 
@@ -106,15 +116,17 @@ import Carousel from "./product _roll/carousel";
                                           {currentSpec <= 0 &&PRITspec.map((item) => (
 
                                             <div className="flex justify-center h-full w-auto hover:scale-125 hover:translate-y-20">
-                                              <img src="https://drive.google.com/uc?export=view&id=1YzxZ2MirclJyI98j0oY1JFISAuIrdCxG"/>
+                                              {/* <img src="https://drive.google.com/uc?export=view&id=1YzxZ2MirclJyI98j0oY1JFISAuIrdCxG"/> */}
                                               {/* <img src={item.imageUrl}/> */}
+                                              <img src={image01}/>
                                             </div>
                                           ))}
                                           {currentSpec > 0 &&INEspec.map((item) => (
 
                                             <div className="flex justify-center h-full w-auto hover:scale-125 hover:translate-y-20">
-                                              <img src="https://drive.google.com/uc?export=view&id=1vc104kTA6ah_F2FuGEicJCsYNKCJR7dm"/>
+                                              {/* <img src="https://drive.google.com/uc?export=view&id=1vc104kTA6ah_F2FuGEicJCsYNKCJR7dm"/> */}
                                               {/* <img src={item.imageUrl}/> */}
+                                              <img src={image02}/>
                                             </div>
                                           ))}
                                         </div>
